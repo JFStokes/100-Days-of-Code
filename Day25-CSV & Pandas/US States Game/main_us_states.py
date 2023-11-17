c9d92c1944c9b56e86c38d2a373660e7e6e5f430
JFStokes <joshua.stokes1617@yahoo.com>
2023-11-16 21:42:41 -0600

    Day25-CSV & Pandas #3

--- Day25-CSV & Pandas/US States Game/main_us_states.py
+++ Day25-CSV & Pandas/US States Game/main_us_states.py
@@ -6,12 +6,55 @@
 #   correct, the name populates on top of the blank state.
 # Date: 15 November 2023        Author: Josh Stokes
 import turtle
-import pandas as p
+import pandas
+from text import Text
 
-t = turtle.Turtle()
+# Variable for the image of the US map.
+map_image = 'blank_states_img.gif'
+
+# Screen setup.
 screen = turtle.Screen()
-data = p.read_csv('50_states.csv')
-print(data)
+screen.title('Josh\'s US States Game')
+screen.addshape(map_image)
+screen.tracer(0)
+turtle.shape(map_image)
+
+# Import data from csv and put States in a list.
+data = pandas.read_csv('50_states.csv')
+state_list = data['state'].to_list()
+
+
+############################ Game Loop ##############################
+game_running = True
+while game_running:
+
+    # Updates the screen at the beginning of each frame.
+    screen.update()
+
+    # Creates popup window for player input.
+    player_answer = screen.textinput(title='Guess the State', 
+                                    prompt='What is another State?')
+    player_answer = player_answer.title()
+    print(player_answer)
+
+    # Commands to end the game.
+    if player_answer == 'Quit' \
+    or player_answer == 'Exit' \
+    or player_answer == 'Stop':
+        game_running = False
+        exit()
+    
+    if player_answer in state_list:
+        print(f'Yes, {player_answer} is one of the states!')
+
+        # Gets x and y of player input.
+        answer_state = data[data.state == player_answer]
+        state_x = int(answer_state.x)
+        state_y = int(answer_state.y)
+        print(f'{player_answer} X is {state_x} and Y is {state_y}')
+        new_state = Text(player_answer, state_x, state_y)
+    else:
+        print(f'Sorry, {player_answer} is not one of the states.')
 
 
 #####################################################################
---
+++ Day25-CSV & Pandas/US States Game/text.py
@@ -0,0 +1,14 @@
+from turtle import Turtle
+
+class Text(Turtle):
+    def __init__(self, name, x_coor, y_coor):
+        super().__init__()
+        self.hideturtle()
+        self.name = name
+        self.xcor = x_coor
+        self.ycor = y_coor
+        self.color('black')
+        self.penup()
+        self.goto(self.xcor, self.ycor)
+        self.write(self.name, False, align='center',
+                   font=('Arial', 8, 'normal'))
