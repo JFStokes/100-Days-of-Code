63049b9c0cecbe2aa06c1dea0a294a08ba12654f
JFStokes <joshua.stokes1617@yahoo.com>
2023-12-06 18:00:45 -0600

    Day28-Tkinter2 #3

--- Day28-Tkinter2 Timer/main.py
+++ Day28-Tkinter2 Timer/main.py
@@ -11,16 +11,44 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 5
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
+timer = None
 
 # ------------------------ TIMER RESET ---------------------------- #
-
+def reset_timer():
+    window.after_cancel(timer)
+    timer_label.config(text='Timer', fg=GREEN)
+    check_mark_label.config(text='')
+    global reps
+    reps = 0
+    tomato_canvas.itemconfig(timer_text, text='00:00')
+    
 # ---------------------- TIMER MECHANISM -------------------------- #
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    print(f'Reps = {reps}')
+    work_sec = WORK_MIN * 1
+    long_break_sec = LONG_BREAK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 1
+
+    # If 1, 3, 5, or 7: work_sec.
+    if reps == 1 or reps == 3 or reps == 5 or reps ==7:
+        count_down(work_sec)
+        timer_label.config(text='Work', fg=GREEN)
+
+    # If 8: long_break:
+    if reps == 8:
+        count_down(long_break_sec)
+        timer_label.config(text='Break', fg=RED)
+
+    # If 2, 4, or 6: short_break:
+    if reps == 2 or reps == 4 or reps == 6:
+        count_down(short_break_sec)
+        timer_label.config(text='Break', fg=PINK)
 
 # -------------------- COUNTDOWN MECHANISM ------------------------ #
 def count_down(count):
@@ -36,7 +64,20 @@ def count_down(count):
 
     tomato_canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+
+    else:
+        # Automatically start the timer again.
+        start_timer()
+        if reps == 2 or reps == 3:
+            check_mark_label.config(text='✔')
+        elif reps == 4 or reps == 5:
+            check_mark_label.config(text='✔✔')
+        elif reps == 6 or reps == 7:
+            check_mark_label.config(text='✔✔✔')
+        else:
+            check_mark_label.config(text='')
 
 # -------------------------- UI SETUP ----------------------------- #
 # Create window.
@@ -64,13 +105,13 @@ start_button.grid(column=0, row=2)
 
 # Create Reset button.
 reset_button = tkinter.Button(font=(FONT_NAME, 8, 'bold'),
-                              text='Reset')
+                              text='Reset', command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 # Create check mark label.
-check_mark_label = tkinter.Label(font=(FONT_NAME, 10, 'bold'),
+check_mark_label = tkinter.Label(font=(FONT_NAME, 14, 'bold'),
                                  background=YELLOW, foreground=GREEN,
-                                 text='✔')
+                                 text='')
 check_mark_label.grid(column=1, row=3)
 
 
