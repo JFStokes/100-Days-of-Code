804a75d2b042af8cecf59d5045bd65cba59bf206
HackerSacker <117953457+JFStokes@users.noreply.github.com>
2024-04-04 21:07:18 -0500

    pwgen 2

--- pwgen.py
+++ pwgen.py
@@ -28,12 +28,13 @@ def check_input(user_input):
 # Display intro messages.
 print("--> Welcome to Josh's Random Password Generator.")
 print("--> Enter the number of upper, lower, integers, and special characters.\n")
+site = input("--> Site/Service: ")
+user_name = input("--> Username: ")
 
 # Function to check for valid input and return random characters.
 def process_input(user_input, list):
     try:
         user_input = int(user_input)
-        print("--> Input accepted.")
     except:
         TypeError
         print("--> Input value must be an integer of 20 or less.")
@@ -42,63 +43,36 @@ def process_input(user_input, list):
             choice = random.choice(upper_list)
             password_list.append(choice)
     elif list == 'l':
-        pass
+        for num in range(user_input):
+            choice = random.choice(lower_list)
+            password_list.append(choice)
     elif list == 'i':
-        pass
+        for num in range(user_input):
+            choice = random.choice(int_list)
+            password_list.append(choice)
     elif list == 's':
-        pass
-    print(password_list)
+        for num in range(user_input):
+            choice = random.choice(spc_list)
+            password_list.append(choice)
         
-num_of_characters = input("--> ")
+num_of_characters = input("--> Num of Uppercase: ")
 process_input(num_of_characters, "u")
 
-num_of_characters = input("--> ")
-process_input(num_of_characters, "u")
-        
-    # check_input(upper_char)
-    # selected_chars = random.choices(upper_list, k=upper_char)
-    # password_list = selected_chars
-    # print(password_list)
+num_of_characters = input("--> Num of Lowercase: ")
+process_input(num_of_characters, "l")
 
-# try:
-#     lower_char = int(input("--> Number of lowercase: "))
-# except:
-#     TypeError
-#     print("--> Input value must be an integer of 20 or less.")
-# check_input(lower_char)
-# selected_chars = random.choices(lower_list, k=lower_char)
-# password_list.append(selected_chars)
-# print(password_list)
+num_of_characters = input("--> Num of Integers: ")
+process_input(num_of_characters, "i")
 
+num_of_characters = input("--> Num of Special Characters: ")
+process_input(num_of_characters, "s")
 
-# try:
-#     int_char = int(input("--> Number of integers: "))
-# except:
-#     TypeError
-#     print("--> Input value must be an integer of 20 or less.")
-# check_input(int_char)
-# selected_chars = random.choices(int_list, k=int_char)
-# password_list.append(selected_chars)
-# print(password_list)
+random.shuffle(password_list)
 
+password_string = ''.join(password_list)
 
-# try:
-#     spc_char = int(input("--> Number of special characters: "))
-# except:
-#     TypeError
-#     print("--> Input value must be an integer of 20 or less.")
-# check_input(spc_char)
-# selected_chars = random.choices(spc_list, k=spc_char)
-# password_list.append(selected_chars)
-# print(password_list)
-
-
-# # Puts the characters in a random order and puts them into a string.
-# random.shuffle(password_list)
-# print(password_list)
-# password_string = ', '.join(password_list)
-
-# # Displays the final password.
-# print(f'--> Your new password is: {password_string}')
+print(f"\n{site}")
+print(f"Username: {user_name}")
+print(f"Password: {password_string}\n")
 
 #####################################################################
