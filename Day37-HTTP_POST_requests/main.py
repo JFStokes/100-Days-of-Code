f243c3d5f298819bc300af3cc703754d5f00a71d
HackerSacker <117953457+JFStokes@users.noreply.github.com>
2024-04-07 13:17:28 -0500

    pwgen 3 & Day37-HTTP_POST_requests

--- Day37-HTTP_POST_requests/main.py
+++ Day37-HTTP_POST_requests/main.py
@@ -2,11 +2,12 @@ import requests
 from datetime import datetime
 
 # Declare constants.
-USERNAME = ""
-TOKEN = ""
-GRAPH_ID = ""
+USERNAME = "jfstokes"
+TOKEN = "xNadW9$a8$0c7#VOy!in"
+GRAPH_ID = "graph1"
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+GRAPH_ENDPOINT2 = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
 
 # Required params for POST request to pixe.la. 
 user_params = {
@@ -16,8 +17,9 @@ user_params = {
     "notMinor": "yes"
 }
 
-# Send POST request.
-response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# Send POST request to create a user account.
+# response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# print(response.text)
 
 graph_config = {
     "id": GRAPH_ID,
@@ -30,3 +32,19 @@ graph_config = {
 headers = {
     "X-USER-TOKEN": TOKEN
 }
+
+# Send POST request to create a graph.
+# response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+# print(response.text)
+
+# Gets today's datetime.
+today = datetime.now()
+
+graph_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "3.0"
+}
+
+# Send POST request to send data to the graph.
+response = requests.post(url=GRAPH_ENDPOINT2, json= graph_data, headers=headers)
+print(response.text)
--- pwgen.py
+++ pwgen.py
@@ -4,32 +4,43 @@
 # Author: Josh Stokes
 # Date: 04 April 2024
 # Purpose: Generate a random password
-
 #####################################################################
+import csv
+import datetime
+import os
 import random
+
+# Set constants.
+DIR_PATH = os.path.dirname(__file__)
+CSV_FILE = DIR_PATH + "\\pw.csv"
+
+# Gets today's date.
+today = datetime.date.today()
 
 # List and final string for password.
 password_list = []
 password_string = ""
 
+#-----------------------> Character Lists <-------------------------#
 # Stores available characters.
 upper_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 lower_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 int_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-spc_list = ['!', '@', '#', '$', '%']
+spc_list = ['!', '@', '#', '$']
+#-------------------------------------------------------------------#
 
 # Function to check input for int less than 21.
-def check_input(user_input):
-    if user_input < 21:
-        print("--> Valid input.")
-    else:
-        print("--> Input value must be an integer of 20 or less.")
+# def check_input(user_input):
+#     if user_input < 21:
+#         print("--> Valid input.")
+#     else:
+#         print("--> Input value must be an integer of 20 or less.")
 
 # Display intro messages.
-print("--> Welcome to Josh's Random Password Generator.")
-print("--> Enter the number of upper, lower, integers, and special characters.\n")
+print("\n--> Welcome to Josh's Random Password Generator.")
 site = input("--> Site/Service: ")
 user_name = input("--> Username: ")
+pw_type = input("--> Manual Password OR Random Password? (M/R): ").upper()
 
 # Function to check for valid input and return random characters.
 def process_input(user_input, list):
@@ -54,25 +65,70 @@ def process_input(user_input, list):
         for num in range(user_input):
             choice = random.choice(spc_list)
             password_list.append(choice)
-        
-num_of_characters = input("--> Num of Uppercase: ")
-process_input(num_of_characters, "u")
 
-num_of_characters = input("--> Num of Lowercase: ")
-process_input(num_of_characters, "l")
+# Create a Manual OR Random password.
+if pw_type == "M":
+    password_string = input("--> Manual Password: ")
 
-num_of_characters = input("--> Num of Integers: ")
-process_input(num_of_characters, "i")
+elif pw_type == "R":
+    num_of_characters = input("--> Num of Uppercase: ")
+    process_input(num_of_characters, "u")
 
-num_of_characters = input("--> Num of Special Characters: ")
-process_input(num_of_characters, "s")
+    num_of_characters = input("--> Num of Lowercase: ")
+    process_input(num_of_characters, "l")
 
-random.shuffle(password_list)
+    num_of_characters = input("--> Num of Integers: ")
+    process_input(num_of_characters, "i")
 
-password_string = ''.join(password_list)
+    num_of_characters = input("--> Num of Special Characters: ")
+    process_input(num_of_characters, "s")
 
-print(f"\n{site}")
+    random.shuffle(password_list)
+
+    password_string = ''.join(password_list)
+else:
+    print("\n--> Invalid input. Please choose M or R.")
+    password_string = "-----"
+
+#-------------------------> Final Output <--------------------------#
+print("\n---------------------------------------")
+print(site)
 print(f"Username: {user_name}")
-print(f"Password: {password_string}\n")
+print(f"Password: {password_string}")
+print("---------------------------------------\n")
+#-------------------------------------------------------------------#
+
+#-----------------------> Add Data to CSV <-------------------------#
+new_data = [site, user_name, password_string, today]
+
+# Check if the CSV file exists
+if os.path.exists(CSV_FILE):
+
+    # If the file exists, open it in append mode
+    with open(CSV_FILE, 'a', newline='') as file:
+
+        # Create a CSV writer object
+        writer = csv.writer(file)
+
+        # Write the new data to the CSV file
+        writer.writerow(new_data)
+
+    print("Data added to existing CSV file.")
+
+else:
+
+    # If the file does not exist, create it and write the data
+    with open(CSV_FILE, 'w', newline='') as file:
+
+        # Create a CSV writer object
+        writer = csv.writer(file)
+
+        # Write the header (if needed)
+        writer.writerow(['Site', 'UserName', 'PassWord', 'Date'])
+
+        # Write the new data to the CSV file
+        writer.writerow(new_data)
+
+    print("CSV file created and data added.")
 
 #####################################################################
