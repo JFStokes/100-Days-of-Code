4f0daa7123b6275518d98345a85f47c5ad08463d
HackerSacker <117953457+JFStokes@users.noreply.github.com>
2024-04-14 11:14:09 -0500

    Day38#2-TODO1 & TODO2

--- Day38-Workout_Tracking & Google Sheets/main.py
+++ Day38-Workout_Tracking & Google Sheets/main.py
@@ -1,17 +1,36 @@
 #####################################################################
 #                         WORKOUT TRACKER                           #
 #####################################################################
+# OVERVIEW - A workout tracker using Nutritionix API and Google Sheets.
 import requests
 
-# Constants
+# TODO 1 - Setup API Credentials and Google Sheet.
+NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 USER_NAME = "jfstokes"
 API_ID = "0acb2c25"
 API_KEY = "1de66b2941723d4cd2eeef769a15177f"
 
-############################## INPUT ################################
+# TODO 2 - Get Exercise Stats with Natural Language Queries.
+exercise = input("--> Exercise: ")
 
-############################ PARAMETERS #############################
+headers = {
+    "x-app-id": API_ID,
+    "x-app-key": API_KEY
+}
 
-############################# REQUEST ###############################
+parameters = {
+    "query": exercise
+}
+
+response = requests.get(url=NUTRITIONIX_ENDPOINT, params=parameters, headers=headers)
+print(response.text)
+
+# TODO 3 - Setup Google Sheet with Sheety.
+
+# TODO 4 - Save data to Google Sheet.
+
+# TODO 5 - Authenticate Sheety API.
+
+# TODO 6 - Environment Variables in Repl.it.
 
 #####################################################################
